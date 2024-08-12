ca7abc91d9fe80aaba2bd10ef35209e5171ecb5e
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2024-08-13 01:09:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 def init_db():
     with sqlite3.connect('database.db') as conn:
+        print(f"\033[93mTrying to create new table if not already created:\033[00m")
         conn.execute('''
             CREATE TABLE IF NOT EXISTS confessions (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,15 +18,13 @@ def init_db():
                 date DATE NOT NULL
             )
         ''')
+        print(f"\033[92mTable confessions is ready for use:\033[00m")
         conn.commit()
 
 
-def setup():
-    init_db()
-
-
 def checkid(id):
-    with sqlite3.connect("database.db") as confessions:
+    try:
+        confessions = sqlite3.connect("database.db")
         cursor = confessions.cursor()
         cursor.execute('SELECT id FROM confessions')
         ids = [str(row[0]) for row in cursor.fetchall()]
@@ -33,15 +32,30 @@ def checkid(id):
         if id not in ids:
             return False
         return True
+    except sqlite3.Error as e:
+        print(f"\033[91mAn error occurred:\033[00m {e} ")
+        init_db()
+        return None
+    finally:
+        if confessions:
+            confessions.close()
 
 
 @app.route('/home', methods=['GET'])
 @app.route('/', methods=['GET'])
 def home():
-    with sqlite3.connect("database.db") as confessions: 
+    try:
+        confessions = sqlite3.connect("database.db")
         cursor = confessions.cursor() 
         cursor.execute('SELECT * FROM confessions') 
         data = cursor.fetchall()
+        confessions.commit()
+    except sqlite3.Error as e:
+        print(f"\033[91mAn error occurred:\033[00m {e} ")
+        init_db()
+    finally:
+        if confessions:
+            confessions.close()
     return render_template('index.html', data=data)
 
 
@@ -57,12 +71,19 @@ def Post_Confession():
         confession = request.form['confession']
         password = request.form['password'].encode('utf-8')
         password = bcrypt.hashpw(password, bcrypt.gensalt())
-        if title == "" or confession == "":
+        if title == "" or confession == "" or password == "":
             return redirect("/", code=302)
-        with sqlite3.connect("database.db") as confessions: 
+        try:
+            confessions = sqlite3.connect("database.db")
             cursor = confessions.cursor()
             cursor.execute("INSERT INTO confessions (title, confession,password, date) VALUES (?, ?, ?, ?)", (title, confession, password, datetime.now().strftime('%d/%m/%Y %I:%M:%S %p'))) 
             confessions.commit()
+        except sqlite3.Error as e:
+            print(f"\033[91mAn error occurred:\033[00m {e} ")
+            init_db()
+        finally:
+            if confessions:
+                confessions.close()
         return redirect("/", code=302)
     else:
         return render_template('PostConfession.html')
@@ -71,25 +92,29 @@ def Post_Confession():
 @app.route('/delete', methods=['GET', 'POST'])
 @app.route('/delete/<id>', methods=['GET', 'POST'])
 def delete_confession(id=None):
-    if checkid(id) == False:
-        return redirect("/", code=302)
-    if id == None:
+    idcheck = checkid(id)
+    if idcheck == False or idcheck == None or id == None:
         return redirect("/", code=302)
     if request.method == 'POST':
         enterd_password = request.form['password'].encode('utf-8')
-        connect = sqlite3.connect('database.db')
-        cursor = connect.cursor()
-        cursor.execute('SELECT password FROM confessions WHERE id=?', (id,))
-        password = cursor.fetchone() 
-        
-        if password and bcrypt.checkpw(enterd_password, password[0]):
-            cursor.execute('DELETE FROM confessions WHERE id=?', (id,))
-            connect.commit()
-            connect.close()
-            return redirect("/", code=302)
-        else:
-            connect.close()
-            return "Password incorrect", 403
+        try:
+            confessions = sqlite3.connect("database.db")
+            cursor = confessions.cursor()
+            cursor.execute('SELECT password FROM confessions WHERE id=?', (id,))
+            password = cursor.fetchone()
+            confessions.commit()
+            if password and bcrypt.checkpw(enterd_password, password[0]):
+                cursor.execute('DELETE FROM confessions WHERE id=?', (id,))
+                confessions.commit()
+                return redirect("/", code=302)
+            else:
+                return "Password incorrect", 403
+        except sqlite3.Error as e:
+            print(f"\033[91mAn error occurred:\033[00m {e} ")
+            init_db()
+        finally:
+            if confessions:
+                confessions.close()
     else:
         return render_template("delete_confession.html")
 
@@ -97,17 +122,22 @@ def delete_confession(id=None):
 @app.route('/confession', methods=['GET'])
 @app.route('/confession/<id>', methods=['GET'])
 def seeing_confession(id=None):
-    if checkid(id) == False:
-        return redirect("/", code=302)
-    if id == None:
+    idcheck = checkid(id)
+    if idcheck == False or idcheck == None or id == None:
         return redirect("/", code=302)
     else:
-        connect = sqlite3.connect('database.db') 
-        cursor = connect.cursor() 
-        cursor.execute('SELECT * FROM confessions WHERE id=?', (id,)) 
-        confession = cursor.fetchone()
-        connect.commit()
-        connect.close()
+        try:
+            confessions = sqlite3.connect("database.db")
+            cursor = confessions.cursor() 
+            cursor.execute('SELECT * FROM confessions WHERE id=?', (id,)) 
+            confession = cursor.fetchone()
+            confessions.commit()
+        except sqlite3.Error as e:
+            print(f"\033[91mAn error occurred:\033[00m {e} ")
+            init_db()
+        finally:
+            if confessions:
+                confessions.close()
         return render_template("confession.html", confession=confession)
 
 
@@ -117,5 +147,5 @@ def Handling_nothing(nothing):
 
 
 if __name__ == '__main__':
-    setup()
+    init_db()
     app.run(debug=True, host="0.0.0.0")
